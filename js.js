3844b17091221357764e7047b0f96fec6f35ee8e
nyrican466 <edwin.alvarado@hotmail.comgit config -1git config --global user.email edwin.alvarado@hotmail.com>
2016-02-15 20:54:41 -0500

    changed syntax for color changes

--- js.js
+++ js.js
@@ -1,39 +1,41 @@
 $(document).ready(function(){
 
-  setInterval(function(){
-  	
-  	var dateTime = new Date();
-	var minutesTwoDigitsWithLeadingZero = ("0" +dateTime.getMinutes()).substr(-2);
-		$("#milliseconds").text(dateTime.getMilliseconds());
-		$("#seconds").text(dateTime.getSeconds());
-		$("#minutes").text(dateTime.getMinutes());
-		$("#hours").text(dateTime.getHours());
-	
-	var thisSecond = dateTime.getSeconds();
-		if (thisSecond <10){
-			thisSecond = "0"+thisSecond;
-
+	setInterval(function(){
+    
+    var dateTime = new Date();
+    var minutesTwoDigitsWithLeadingZero = ("0" +dateTime.getMinutes()).substr(-2);
+    
     var thisMinute = dateTime.getMinutes();
     var thisHour = dateTime.getHours();
-	}
-    $("#minutes").text(thisMinute);
-	$("#seconds").text(thisSecond);
-	$("#minutes").text(dateTime.getMinutes());
-		if (thisSecond%2==0) {
-			$("body").css("background-color","blue");
-	} 
-	else {
-		    $("body").css("background-color", "purple")
-	}
+    var thisSecond = dateTime.getSeconds();
+    var thisMilliseconds = dateTime.getMilliseconds();
+	  
+	    $("#hours").text(thisHour);
+	    $("#min").text(thisMinute);
+	    $("#seconds").text(thisSecond);
+	    $("#milliseconds").text(thisMilliseconds);
 
-	    if (thisMinute%5==0) {
-	    	$("body").css("background-color","yellow")
-	    }
+      if (thisSecond < 10){
+      thisSecond = "0" + thisSecond;
+      }
+        
+      if (thisMinute%2=== 0) { 
+        $("#min").css("color", "blue")
 
-	    if (thisHour%1==0) {
-	    	$("clock").css("color", "black")
-	    }
-		
-	},1);
+    } else {
+        $("#min").css("color", "black");
+      }
+
+
+     if (thisMinute%5==0); {
+        $("body").css("background-color", "yellow");
+      }
+
+     if (thisHour % 1 === 0); {
+        $("#clock").css("color", "purple");
+      } 
+    
+  }, 1);
+
 
 });
